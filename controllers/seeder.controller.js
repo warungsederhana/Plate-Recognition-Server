a151c84df94b00bbd2fc885a7b89277c78c9c963
gybran <charistomarcgybran@students.undip.ac.id>
2024-02-24 13:49:03 +0700

    feat: seeder telah di update

--- controllers/seeder.controller.js
+++ controllers/seeder.controller.js
@@ -1,61 +1,406 @@
 const { faker } = require("@faker-js/faker");
 const kendaraanSchema = require("../Schemas/kendaraan.schema");
+const negaraSchema = require("../Schemas/negaraAsal.schema");
+const merkSchema = require("../Schemas/merkKendaraan.schema");
+const jenisSchema = require("../Schemas/jenisKendaraan.schema");
+const typeSchema = require("../Schemas/typeKendaraan.schema");
 const tagihanPajakSchema = require("../Schemas/tagihanPajak.schema");
 const admin = require("../lib/firebase/admin");
 const db = admin.firestore();
 
-exports.kendaraanSeed = async (req, res) => {
+const convertUSDToIDR = (usd, exchangeRate = 14500) => {
+  return usd * exchangeRate;
+};
+
+const addYearsToDate = (date, yearsToAdd) => {
+  const newDate = new Date(date);
+  newDate.setFullYear(newDate.getFullYear() + yearsToAdd);
+  return newDate;
+};
+
+const formatIsoDateToDDMMYYYY = (isoDateString, yearsToAdd = 0) => {
+  const date = new Date(isoDateString);
+  date.setFullYear(date.getFullYear() + yearsToAdd); // Adding the years
+
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are zero-indexed
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+exports.negaraSeed = async (req, res) => {
+  const collectionName = "NegaraAsal";
+  const countries = new Set();
+  const countryCodes = new Set();
+  const seeds = [];
+
+  while (seeds.length < 10) {
+    const countryName = faker.location.country();
+    const countryCode = faker.location.countryCode("numeric");
+
+    // Check if country name and country code are unique
+    if (!countries.has(countryName) && !countryCodes.has(countryCode)) {
+      countries.add(countryName);
+      countryCodes.add(countryCode);
+      seeds.push({ nama_negara: countryName, kode_negara: countryCode });
+    }
+  }
+
+  console.log(seeds);
+
   try {
-    const collectionName = "Kendaraan";
-    const users = await db.collection("Users").get();
-    const userIds = users.docs.map((doc) => doc.id);
-
-    const kendaraans = Array.from({ length: 10 }, () => {
-      const userId = faker.helpers.arrayElement(userIds);
-      const user = users.docs.find((doc) => doc.id === userId);
-
-      return {
-        id_user: userId,
-        nama: faker.vehicle.vehicle(),
-        nama_pemilik: user.data().nama,
-        nama_pemilik_lama: faker.person.fullName(),
-        jenis: faker.vehicle.model(),
-        tipe: faker.vehicle.type(),
-        nomor_rangka: faker.vehicle.vin(),
-        nomor_mesin: faker.vehicle.vin(),
-        nomor_bpkb: faker.vehicle.vin(),
-        nomor_polisi: faker.vehicle.vrm(),
-        warna: faker.vehicle.color(),
-        tahun_pembuatan: faker.date.past().getFullYear(),
-        bahan_bakar: faker.vehicle.fuel(),
-      };
-    });
-
-    console.log(`Seeding ${collectionName}...`);
     const collectionRef = db.collection(collectionName);
     const batch = db.batch();
 
-    for (const object of kendaraans) {
-      console.log(`Seeding kendaraan ${object.nama}...`);
+    for (const seed of seeds) {
+      console.log(`Seeding negara ${seed.nama_negara}...`);
       const docRef = collectionRef.doc();
-      const userRef = db.collection("Users").doc(object.id_user);
 
-      const newKendaraan = {
+      const negara = {
         id: docRef.id,
-        ...object,
+        ...seed,
       };
 
-      const verifyKendaraan = kendaraanSchema.safeParse(newKendaraan);
-      if (verifyKendaraan.success) {
+      const verifyNegara = negaraSchema.safeParse(negara);
+
+      if (verifyNegara.success) {
         batch.set(docRef, {
-          ...verifyKendaraan.data,
+          ...negara,
           createdAt: new Date().toISOString(),
           updatedAt: new Date().toISOString(),
         });
+      }
+    }
 
-        batch.update(userRef, {
-          kendaraans: admin.firestore.FieldValue.arrayUnion(docRef.id),
+    await batch.commit();
+    console.log(`Seeding ${collectionName} finished!`);
+
+    res.status(200).send({
+      success: true,
+      message: "Negara seeded successfully",
+    });
+  } catch (error) {
+    console.log("Error seeding negara", error.message);
+    res.status(500).send({
+      success: false,
+      message: "Error seeding kendaraan",
+      error: error.message,
+    });
+  }
+};
+
+exports.merkSeed = async (req, res) => {
+  const collectionName = "MerkKendaraan";
+  const merks = new Set();
+  const merkIds = new Set();
+  const seeds = [];
+
+  try {
+    const negaraAsal = await db.collection("NegaraAsal").get();
+    const negaraAsalCodes = negaraAsal.docs.map((doc) => doc.data().kode_negara);
+
+    while (seeds.length < 10) {
+      const merkName = faker.vehicle.manufacturer();
+      const merkId = faker.string.numeric({ length: 3 });
+      const kodeNegara = faker.helpers.arrayElement(negaraAsalCodes);
+
+      // Check if merk name and merk ID are unique
+      if (!merks.has(merkName) && !merkIds.has(merkId)) {
+        merks.add(merkName);
+        merkIds.add(merkId);
+        seeds.push({ id: merkId, nama_merk: merkName, kode_negara_asal: kodeNegara });
+      }
+    }
+
+    const collectionRef = db.collection(collectionName);
+    const batch = db.batch();
+
+    for (const seed of seeds) {
+      console.log(`Seeding merk ${seed.nama_merk}...`);
+      const docRef = collectionRef.doc();
+
+      const verifyMerk = merkSchema.safeParse(seed);
+      if (verifyMerk.success) {
+        batch.set(docRef, {
+          ...seed,
+          createdAt: new Date().toISOString(),
           updatedAt: new Date().toISOString(),
+        });
+      }
+    }
+    await batch.commit();
+    console.log(`Seeding ${collectionName} finished!`);
+
+    res.status(200).send({
+      success: true,
+      message: "Merk seeded successfully",
+    });
+  } catch (error) {
+    console.log("Error seeding merk", error.message);
+    res.status(500).send({
+      success: false,
+      message: "Error seeding merk",
+      error: error.message,
+    });
+  }
+};
+
+exports.jenisSeed = async (req, res) => {
+  const collectionName = "JenisKendaraan";
+  const jenis = new Set();
+  const jenisIds = new Set();
+  const seeds = [];
+
+  while (seeds.length < 11) {
+    const jenisName = faker.vehicle.type();
+    const jenisId = faker.string.numeric({ length: 3 });
+    const jumlahSumbu = faker.number.int({ min: 4, max: 8 }).toString();
+
+    // Generate kode jenis kendaraan
+    const jenisKode =
+      jenisName.split(" ").length > 1
+        ? jenisName
+            .split(" ")
+            .map((word) => word[0])
+            .join("")
+        : jenisName.substring(0, 2).toUpperCase();
+
+    // Check if jenis name and jenis ID are unique
+    if (!jenis.has(jenisName) && !jenisIds.has(jenisId)) {
+      jenis.add(jenisName);
+      jenisIds.add(jenisId);
+      seeds.push({
+        id: jenisId,
+        nama_jenis_kendaraan: jenisName,
+        kode_jenis_kendaraan: jenisKode,
+        jumlah_sumbu: jumlahSumbu,
+      });
+    }
+  }
+
+  try {
+    const collectionRef = db.collection(collectionName);
+    const batch = db.batch();
+
+    for (const seed of seeds) {
+      console.log(`Seeding jenis ${seed.nama_jenis_kendaraan}...`);
+      const docRef = collectionRef.doc();
+
+      const verifyJenis = jenisSchema.safeParse(seed);
+      if (verifyJenis.success) {
+        batch.set(docRef, {
+          ...seed,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        });
+      }
+    }
+
+    await batch.commit();
+    console.log(`Seeding ${collectionName} finished!`);
+
+    res.status(200).send({
+      success: true,
+      message: "Jenis kendaraan seeded successfully",
+    });
+  } catch (error) {
+    console.log("Error seeding jenis kendaraan", error.message);
+    res.status(500).send({
+      success: false,
+      message: "Error seeding jenis kendaraan",
+      error: error.message,
+    });
+  }
+};
+
+exports.typeSeed = async (req, res) => {
+  const collectionName = "TypeKendaraan";
+  const types = new Set();
+  const typeIds = new Set();
+  const seeds = [];
+
+  try {
+    const jenisKendaraan = await db.collection("JenisKendaraan").get();
+    const jenisKendaraanIds = jenisKendaraan.docs.map((doc) => doc.data().id);
+    const merkKendaraan = await db.collection("MerkKendaraan").get();
+    const merkKendaraanIds = merkKendaraan.docs.map((doc) => doc.data().id);
+    const negaraAsal = await db.collection("NegaraAsal").get();
+    const negaraAsalCodes = negaraAsal.docs.map((doc) => doc.data().kode_negara);
+
+    while (seeds.length < 10) {
+      const typeName = faker.vehicle.model();
+      const typeId = faker.string.numeric({ length: 3 });
+      const typeNameEri = typeName.split(" ").join("_").toUpperCase();
+      const kodeNegara = faker.helpers.arrayElement(negaraAsalCodes);
+      const jenisKendaraanId = faker.helpers.arrayElement(jenisKendaraanIds);
+      const merkKendaraanId = faker.helpers.arrayElement(merkKendaraanIds);
+
+      // Check if type name and type ID are unique
+      if (!types.has(typeName) && !typeIds.has(typeId)) {
+        types.add(typeName);
+        typeIds.add(typeId);
+        seeds.push({
+          id: typeId,
+          nama_type_kendaraan: typeName,
+          nama_type_kendaraan_eri: typeNameEri,
+          id_jenis_kendaraan: jenisKendaraanId,
+          id_merk_kendaraan: merkKendaraanId,
+          kode_negara_asal: kodeNegara,
+        });
+      }
+    }
+
+    const collectionRef = db.collection(collectionName);
+    const batch = db.batch();
+
+    for (const seed of seeds) {
+      console.log(`Seeding type ${seed.nama_type_kendaraan}...`);
+      const docRef = collectionRef.doc();
+
+      const verifyType = typeSchema.safeParse(seed);
+      if (verifyType.success) {
+        batch.set(docRef, {
+          ...seed,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+        });
+      }
+    }
+
+    await batch.commit();
+    console.log(`Seeding ${collectionName} finished!`);
+
+    res.status(200).send({
+      success: true,
+      message: "Type kendaraan seeded successfully",
+    });
+  } catch (error) {
+    console.log("Error seeding type kendaraan", error.message);
+    res.status(500).send({
+      success: false,
+      message: "Error seeding type kendaraan",
+      error: error.message,
+    });
+  }
+};
+
+exports.kendaraanSeed = async (req, res) => {
+  const collectionName = "Kendaraan";
+  const seeds = [];
+
+  try {
+    const jenisKendaraan = await db.collection("JenisKendaraan").get();
+    const jenisKendaraanIds = jenisKendaraan.docs.map((doc) => doc.data().id);
+    const merkKendaraan = await db.collection("MerkKendaraan").get();
+    const merkKendaraanIds = merkKendaraan.docs.map((doc) => doc.data().id);
+    const typeKendaraan = await db.collection("TypeKendaraan").get();
+    const typeKendaraanIds = typeKendaraan.docs.map((doc) => doc.data().id);
+
+    while (seeds.length < 10) {
+      const id_jenis_kendaraan = faker.helpers.arrayElement(jenisKendaraanIds);
+      const id_merk_kendaraan = faker.helpers.arrayElement(merkKendaraanIds);
+      const id_type_kendaraan = faker.helpers.arrayElement(typeKendaraanIds);
+
+      const tgl_faktur_kwitansi = faker.date.recent();
+      const tgl_stnk = faker.date.recent();
+      const tgl_jth_tempo = faker.date.recent();
+      const kendaraan = {
+        no_daftar: faker.string.numeric(10),
+        no_daftar_eri: faker.string.numeric(10),
+        id_kepemilikan: faker.string.numeric(16),
+        no_kk: faker.string.numeric(16),
+        no_polisi: faker.vehicle.vrm(),
+        no_polisi_lama: faker.vehicle.vrm(),
+        nama_pemilik: faker.person.fullName(),
+        nama_pemilik_lama: faker.person.fullName(),
+        alamat1: faker.location.streetAddress(),
+        alamat2: faker.location.streetAddress(),
+        id_kelurahan: faker.string.numeric(10),
+        no_telp: faker.phone.number(),
+        id_jenis_kendaraan: id_jenis_kendaraan,
+        id_merk_kendaraan: id_merk_kendaraan,
+        id_type_kendaraan: id_type_kendaraan,
+        id_model_kendaraan: id_jenis_kendaraan + id_merk_kendaraan + id_type_kendaraan,
+        // id_jenis_map: faker.random.uuid(),
+        tahun_buat: faker.number.int({ min: 1980, max: 2021 }),
+        tahun_rakit: faker.number.int({ min: 1980, max: 2021 }),
+        tahun_ub: faker.number.int({ min: 1980, max: 2021 }),
+        cylinder: faker.number.int({ min: 1000, max: 5000 }),
+        // id_golongan_kendaraan: faker.random.uuid(),
+        // id_warna_tnkb: faker.datatype.number(),
+        warna_kendaraan: faker.vehicle.color(),
+        // id_lokasi: faker.random.uuid(),
+        // dati2_induk: faker.random.word(),
+        // id_fungsi_kendaraan: faker.datatype.number(),
+        // id_bahan_bakar: faker.datatype.number(),
+        no_rangka: faker.vehicle.vin(),
+        no_mesin: faker.vehicle.vin(),
+        no_bpkb: faker.vehicle.vrm(),
+        jumlah_sumbu: faker.number.int({ min: 2, max: 5 }),
+        // kode_jenis: faker.random.word(),
+        status_blokir: faker.datatype.boolean(),
+        progresif: faker.number.int(),
+        progresif_tarif: faker.number.int(),
+        // id_pendaftaran: faker.random.uuid(),
+        // id_lokasi_proses: faker.random.uuid(),
+        // dati2_proses: faker.random.word(),
+        // tujuan_mutasi: faker.random.word(),
+        tanggal_faktur: tgl_faktur_kwitansi,
+        tanggal_kwitansi: tgl_faktur_kwitansi,
+        tanggal_akhir_stnk: addYearsToDate(tgl_stnk, 5),
+        tanggal_akhir_stnk_lama: tgl_stnk,
+        tanggal_jatuh_tempo: addYearsToDate(tgl_jth_tempo, 1),
+        tanggal_jatuh_tempo_lama: tgl_jth_tempo,
+        // id_status: faker.random.uuid(),
+        bb1_pokok: convertUSDToIDR(faker.finance.amount({ min: 10, max: 15, dec: 2 })),
+        bb1_denda: convertUSDToIDR(faker.finance.amount({ min: 0, max: 5, dec: 2 })),
+        pkb_pokok: convertUSDToIDR(faker.finance.amount({ min: 10, max: 15, dec: 2 })),
+        pkb_denda: convertUSDToIDR(faker.finance.amount({ min: 0, max: 5, dec: 2 })),
+        pkb_bunga: convertUSDToIDR(faker.finance.amount({ min: 0, max: 5, dec: 2 })),
+        swdkllj_pokok: convertUSDToIDR(faker.finance.amount({ min: 10, max: 15, dec: 2 })),
+        swdkllj_denda: convertUSDToIDR(faker.finance.amount({ min: 0, max: 5, dec: 2 })),
+        stnk: convertUSDToIDR(faker.finance.amount({ min: 10, max: 15, dec: 2 })),
+        no_skpd: faker.string.numeric(10),
+        no_kohir: faker.string.alphanumeric(15),
+        no_skum: faker.string.alphanumeric(15),
+        // tanggal_daftar: faker.date.past(),
+        // tanggal_daftar: faker.date.past(),
+        tahun_berlaku: faker.number.int({ min: 2023, max: 2025 }),
+        tanggal_max_bayar_pkb: faker.date.future(),
+        tanggal_max_bayar_swdkllj: faker.date.future(),
+        // kode_pembayaran: faker.random.alphanumeric(10),
+        // dpwkp: faker.datatype.number(),
+        // ket_dpwkp: faker.random.word(),
+        // tanggal_jatuh_tempo_dpwkp: faker.date.future(),
+        subsidi: faker.datatype.boolean(),
+        njkb: convertUSDToIDR(faker.finance.amount({ min: 20, max: 50 })),
+      };
+
+      seeds.push(kendaraan);
+    }
+
+    const collectionRef = db.collection(collectionName);
+    const batch = db.batch();
+
+    for (const seed of seeds) {
+      console.log(`Seeding kendaraan ${seed.id_kepemilikan}...`);
+      const docRef = collectionRef.doc();
+
+      const kendaraan = {
+        id: docRef.id,
+        ...seed,
+      };
+
+      const verifyKendaraan = kendaraanSchema.safeParse(kendaraan);
+
+      if (!verifyKendaraan.success) throw new Error(verifyKendaraan.error);
+
+      if (verifyKendaraan.success) {
+        batch.set(docRef, {
+          ...seed,
+          createdAt: new Date(),
+          updatedAt: new Date(),
         });
       }
     }
@@ -68,6 +413,7 @@ exports.kendaraanSeed = async (req, res) => {
       message: "Kendaraan seeded successfully",
     });
   } catch (error) {
+    console.log("Error seeding kendaraan", error.message);
     res.status(500).send({
       success: false,
       message: "Error seeding kendaraan",
--- routes/seeder.route.js
+++ routes/seeder.route.js
@@ -4,5 +4,9 @@ const router = express.Router();
 
 router.get("/kendaraan", controller.kendaraanSeed);
 router.get("/tagihan-pajak", controller.tagihanPajakSeed);
+router.get("/negara-asal", controller.negaraSeed);
+router.get("/merk", controller.merkSeed);
+router.get("/jenis-kendaraan", controller.jenisSeed);
+router.get("/type-kendaraan", controller.typeSeed);
 
 module.exports = router;
